15eb541c9a2408f25a803d04bb53d29af9c73b86
Nominal9977 <84743651+Nominal9977@users.noreply.github.com>
2025-10-14 13:42:41 -0400

    new null case

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -4,25 +4,23 @@
 
 Point2D Cat::Move(World* world) {
   auto path = generatePath(world);
-  if(path.size() == 0) {
-    auto rand = Random::Range(0, 5);
-    auto pos = world->getCat();
-    switch (rand) {
-      case 0:
-        return World::NE(pos);
-      case 1:
-        return World::NW(pos);
-      case 2:
-        return World::E(pos);
-      case 3:
-        return World::W(pos);
-      case 4:
-        return World::SW(pos);
-      case 5:
-        return World::SE(pos);
-      default:
-        throw "random out of range";
+  if (path.empty()) {
+    Point2D catPos = world->getCat();
+    auto catNeighbors = World::neighbors(catPos);
+    std::vector<Point2D> validPositions;
+
+    for (const auto& n : catNeighbors) {
+      if (world->catcherCanMoveToPosition(n) && !world->getContent(n)) {
+        validPositions.push_back(n);
+      }
     }
+
+    if (!validPositions.empty()) {
+      int randomIndex = Random::Range(0, (int)validPositions.size() - 1);
+      return validPositions[randomIndex];
+    }
+
+    return Point2D::INFINITE;
   }
   return path[0];
 }
